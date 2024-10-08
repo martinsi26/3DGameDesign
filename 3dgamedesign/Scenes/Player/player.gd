af49f9a84946d745241517a78009c4c78cbe2942
martinsi26 <martinsi26@up.edu>
2024-10-07 19:01:11 -0700

    updated sword movement

--- 3dgamedesign/Scenes/Player/player.gd
+++ 3dgamedesign/Scenes/Player/player.gd
@@ -5,6 +5,12 @@ class_name Player extends CharacterBody3D
 @export var ANIMATION_PLAYER : AnimationPlayer
 @export var SWORD: Node3D
 
+@export var TILT_LOWER_LIMIT := deg_to_rad(-60.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(30.0)
+
+@export var TILT_LEFT_LIMIT := deg_to_rad(-72.5)
+@export var TILT_RIGHT_LIMIT := deg_to_rad(55.0)
+
 var original_sword_position
 var original_sword_rotation
 
@@ -12,12 +18,15 @@ var mouse_captured: bool = false
 
 var current_rotation: float
 var camera_rotation: Vector2
+var sword_rotation: Vector2
+
+var lock_camera = false
 
 var gravity = 12.0
 
 # This function handles user input and input events such as mouse movement
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and mouse_captured:
+	if event is InputEventMouseMotion and !lock_camera:
 		# after checking the mouse motino event we capture the mouse and update the camer to look
 		var mouse_event = event.relative * MOUSE_SENSITIVITY
 		camera_look(mouse_event)
@@ -86,27 +95,46 @@ func default_sword():
 	SWORD.rotation = original_sword_rotation
 
 # update the sword to point at the mouse position
-func update_sword():
+func update_sword(movement):
+	# rotate the sword and move the sword based on mouse movement
+	# similar to the camera_look function, so as I move the mouse
+	# in a direction the sword rotates/shifts its position based
+	# on that mouse movement
+	
+	sword_rotation += movement
+	
+	sword_rotation.y = clamp(sword_rotation.y, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	sword_rotation.x = clamp(sword_rotation.x, TILT_LEFT_LIMIT, TILT_RIGHT_LIMIT)
+	
+	
+	SWORD.transform.basis = Basis()
+	
+	SWORD.rotate_object_local(Vector3(0,1,0), -sword_rotation.x)
+	SWORD.rotate_object_local(Vector3(1,0,0), -sword_rotation.y)
+	
+	
+	
+	
 	# get the current space (3D world)
-	var space_state = get_world_3d().direct_space_state
-	
-	# find the mouse position
-	var mouse_position = get_viewport().get_mouse_position()
-	
-	var ray_length = 2000
-	# cast a ray out of the camera to the mouse
-	var ray_origin = $CameraController/Camera.project_ray_origin(mouse_position)
-	var ray_end = ray_origin + $CameraController/Camera.project_ray_normal(mouse_position) * ray_length
-	
-	# create the ray from the ray origin and ray end
-	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
-	# find the intersection of the 3 dimensional world of the ray and mouse
-	var intersection = space_state.intersect_ray(query)
-	
-	if not intersection.is_empty():
-		var pos = intersection.position
-		# make the sword look at the intersection point
-		SWORD.look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0, 1, 0))
+	#var space_state = get_world_3d().direct_space_state
+	#
+	## find the mouse position
+	#var mouse_position = get_viewport().get_mouse_position()
+	#
+	#var ray_length = 2000
+	## cast a ray out of the camera to the mouse
+	#var ray_origin = $CameraController/Camera.project_ray_origin(mouse_position)
+	#var ray_end = ray_origin + $CameraController/Camera.project_ray_normal(mouse_position) * ray_length
+	#
+	## create the ray from the ray origin and ray end
+	#var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	## find the intersection of the 3 dimensional world of the ray and mouse
+	#var intersection = space_state.intersect_ray(query)
+	#
+	#if not intersection.is_empty():
+		#var pos = intersection.position
+		## make the sword look at the intersection point
+		#SWORD.look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0, 1, 0))
 	
 # update the gravity so the player falls
 func update_gravity(delta) -> void:
--- 3dgamedesign/Scripts/PlayerStateScripts/AttackPlayerState.gd
+++ 3dgamedesign/Scripts/PlayerStateScripts/AttackPlayerState.gd
@@ -1,20 +1,30 @@
 class_name AttackPlayerState extends PlayerActionState
 
+@export var MOUSE_SENSITIVITY = 0.002
+var move_sword = false
+var target
+
 func enter(_previous_state) -> void:
-	PLAYER.release_mouse()
+	target = PLAYER.find_target()
 
 func update(delta):
-	var target = PLAYER.find_target()
 	if target == null:
 		transition.emit("IdleCombatPlayerState", true)
 	else:
+		move_sword = true
+		PLAYER.lock_camera = true
 		PLAYER.camera_follow_enemy(target)
-		PLAYER.update_sword()
 	
 		if Input.is_action_just_pressed("lock"):
 			transition.emit("IdleCombatPlayerState", true)
 	
 func exit() -> void:
 	PLAYER.default_sword()
-	PLAYER.capture_mouse()
+	#PLAYER.capture_mouse()
+	move_sword = false
+	PLAYER.lock_camera = false
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and move_sword:
+		var mouse_event = event.relative * MOUSE_SENSITIVITY
+		PLAYER.update_sword(mouse_event)
