485e91a35e7701e2f3b8c19c34ce7773456a1331
Chris Yee <yeec26@up.edu>
2024-10-23 15:37:33 -0700

    before i mess something up

--- 3dgamedesign/Scenes/Enemy/Boar.gd
+++ 3dgamedesign/Scenes/Enemy/Boar.gd
@@ -1,10 +1,15 @@
 class_name Boar extends Enemy
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var state_machine = $AnimationTree["parameters/playback"]
 
 # move the enemy to point towards the player
 func enemy_follow_player(player) -> void:
-	#self.look_at(player.global_position)
 	self.look_at(Vector3(player.global_position.x, 0, player.global_position.z), Vector3(0, 1, 0), true)
 	self.rotate_y(30)
+	self.axis_lock_angular_y
 	self.orthonormalize()
+	
+# conditions
+func _process(delta: float) -> void:
+	move_and_slide()
