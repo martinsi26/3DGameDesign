c231864e7358ab3cc41582cc98bc4876717e9b53
Chris Yee <yeec26@up.edu>
2024-10-22 20:54:43 -0700

    did a whoopsies

--- 3dgamedesign/Scenes/Enemy/Boar.gd
+++ 3dgamedesign/Scenes/Enemy/Boar.gd
@@ -1,5 +1,6 @@
-extends Node3D
+class_name Boar extends CharacterBody3D
 
+@onready var navigation_agent: NavigationAgent3D = get_node("NavigationAgent3D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
