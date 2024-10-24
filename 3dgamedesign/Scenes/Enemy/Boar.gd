cc93b89d0cc6014e30d4f508b3970cc33b2e9e59
Chris Yee <yeec26@up.edu>
2024-10-23 20:37:57 -0700

    FiNAL 4 si -mon

--- 3dgamedesign/Scenes/Enemy/Boar.gd
+++ 3dgamedesign/Scenes/Enemy/Boar.gd
@@ -6,9 +6,8 @@ class_name Boar extends Enemy
 func enemy_follow_player(player) -> void:
 	self.look_at(Vector3(player.global_position.x, 0, player.global_position.z), Vector3(0, 1, 0), true)
 	self.rotate_y(30)
-	self.axis_lock_angular_y
 	self.orthonormalize()
-	
-# conditions
+	self.axis_lock_angular_y = true
+
 func _process(delta: float) -> void:
 	move_and_slide()
