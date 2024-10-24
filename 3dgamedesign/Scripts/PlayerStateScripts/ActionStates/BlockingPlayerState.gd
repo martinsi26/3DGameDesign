e094b9f4a9edcdf11b48c42489235846eb002839
HenrySchiffUP <schiff26@up.edu>
2024-10-23 19:26:18 -0700

    Organized player's action and movements states

--- 3dgamedesign/Scripts/PlayerStateScripts/ActionStates/BlockingPlayerState.gd
+++ 3dgamedesign/Scripts/PlayerStateScripts/ActionStates/BlockingPlayerState.gd
@@ -8,7 +8,8 @@ func enter(_previous_state) -> void:
 	pass
 
 func update(delta):
-	pass
+	if Input.is_action_just_released("block"):
+		transition.emit("LockingPlayerState", true)
 	
 func exit() -> void:
 	pass
